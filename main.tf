0d16041505d0c1cca6aea1e3359e41e068d4accc
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-02-22 22:37:02 +0700

    add awscc provider and RDS DB instance resource to main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.17.0"
     }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "1.60.0"
+    }
   }
 }
 
@@ -20,4 +24,16 @@ resource "aws_subnet" "subnet01" {
 resource "aws_subnet" "subnet02" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.subnet[1]
+}
+
+resource "awscc_rds_db_instance" "main_db" {
+  db_name = "jetbrains_product_version"
+  engine = "postgres"
+
+  allocated_storage = "20"
+
+  db_instance_class = "db.t4g.micro"
+
+  master_username = "nawaphon"
+  master_user_password = true
 }
