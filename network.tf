6dc45ecb1b6db2aae768468a79f3f1c122701155
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-02-22 22:05:38 +0700

    remove locals block and update CICD variable definition in network.tf

--- network.tf
+++ network.tf
@@ -1,16 +1,5 @@
 
 
-locals {
-  vpc_ip = "192.168.0.0"
-  network_bit = 16
-
-  network_bit_for_subnet = 28
-
-  cicd = "${local.vpc_ip}/${local.network_bit}"
-
-  avaliable_subnets = cidrsubnets(cicd, local.network_bit_for_subnet - local.network_bit)
-}
-
 variable "CICD" {
   type = string
   default = "192.168.0.0/16"
